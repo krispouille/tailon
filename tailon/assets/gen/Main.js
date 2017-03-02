feee2a38b79b85aa574ebd4c714baf23fc96b261
Chrislain Jemba <cjemba@autopassgroup.com>
2017-03-02 16:26:05 +0100

    fix regex for bash colors

--- tailon/assets/gen/Main.js
+++ tailon/assets/gen/Main.js
@@ -26,7 +26,7 @@ var LogView = (function () {
         else if (/\[WARN\]/.test(inner_html)){
             class_names += ' log-warn';
         }
-        inner_html = inner_html.replace(/\[[^m]+m/, '');
+        inner_html = inner_html.replace(/\[\d+m/g, '');
         span.innerHTML = inner_html;
         span.className = class_names;
         return span;
